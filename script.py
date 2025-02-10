57bd5217012931da5ffca07dde3909507f46563e
testhesabi-git <githubtesthesabi@gmail.com>
2025-02-10 17:56:03 +0000

    Hatali girişleri kontrol eden hata mesajları eklendi. Fixes #3 (#5)

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
+def sayi_al(mesaj):
+    while True:
+        try:
+            return float(input(mesaj))
+        except ValueError:
+            print("Hata: Lütfen geçerli bir sayı girin!")
+
 while True:
     print("1. Toplama")
     print("2. Çıkarma")
@@ -8,23 +15,23 @@ while True:
     secim = input("Bir işlem seçin: ")
 
     if secim == "1":
-        sayi1 = float(input("Birinci sayıyı girin: "))
-        sayi2 = float(input("İkinci sayıyı girin: "))
+        sayi1 = sayi_al("Birinci sayıyı girin: ")
+        sayi2 = sayi_al("İkinci sayıyı girin: ")
         print("Sonuç:", sayi1 + sayi2)
     elif secim == "2":
-        sayi1 = float(input("Birinci sayıyı girin: "))
-        sayi2 = float(input("İkinci sayıyı girin: "))
+        sayi1 = sayi_al("Birinci sayıyı girin: ")
+        sayi2 = sayi_al("İkinci sayıyı girin: ")
         print("Sonuç:", sayi1 - sayi2)
     elif secim == "3":
-        sayi1 = float(input("Birinci sayıyı girin: "))
-        sayi2 = float(input("İkinci sayıyı girin: "))
+        sayi1 = sayi_al("Birinci sayıyı girin: ")
+        sayi2 = sayi_al("İkinci sayıyı girin: ")
         print("Sonuç:", sayi1 * sayi2)
     elif secim == "4":
-        sayi1 = float(input("Birinci sayıyı girin: "))
-        sayi2 = float(input("İkinci sayıyı girin: "))
+        sayi1 = sayi_al("Birinci sayıyı girin: ")
+        sayi2 = sayi_al("İkinci sayıyı girin: ")
         print("Sonuç:", sayi1 / sayi2)
     elif secim == "5":
-        sayi1 = float(input("Sayı girin: "))
+        sayi1 = sayi_al("Sayıyı girin: ")
         print("Sonuç", sayi1 * sayi1)
     else:
         print("Lütfen geçerli bir seçim yapın.(1-4)")
